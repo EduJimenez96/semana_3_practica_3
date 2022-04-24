b0c8e9c98f5bb66358eea83af62aabcea76af625
EduJimenez96 <ejimenez10@ucol.mx>
2022-04-24 13:27:39 -0500

    Requirement 2

--- sprint_1/twitter/test/models/user.test.js
+++ sprint_1/twitter/test/models/user.test.js
@@ -2,13 +2,13 @@ const User = require('../../app/models/User')
 
 describe("Unit tests for User class", () => {
   test("Create an User object", () => {
-    const user = new User(1, "Lalo_JimCi96", "Eduardo", "Bio", "dateCreated", "lastUpdated")
+    const user = new User(1, "Lalo_JimCi96", "Eduardo", "Bio")
 
     expect(user.id).toBe(1)
     expect(user.username).toBe("Lalo_JimCi96")
     expect(user.name).toBe("Eduardo")
     expect(user.bio).toBe("Bio")
-    expect(user.dateCreated).toBe("dateCreated")
-    expect(user.lastUpdated).toBe("lastUpdated")
+    expect(user.dateCreated).not.toBeUndefined()
+    expect(user.lastUpdated).not.toBeUndefined()
   });
 })
