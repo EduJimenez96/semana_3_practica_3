457292b4cadcca044bc1de1e076181ebe3a502f8
EduJimenez96 <ejimenez10@ucol.mx>
2022-04-24 16:20:05 -0500

    Sprint 3 - Requirement 2

--- sprint_1/twitter/app/views/UserView.js
+++ sprint_1/twitter/app/views/UserView.js
@@ -1,15 +1,25 @@
 const User = require("../models/User")
 
 class UserView {
-  constructor(error) {
-    this.error = error
-  }
   static createUser(payload) {
-    if (payload == null) { 
-      this.error = "payload no existe"
-      return this
+    var error = this.#checkPayload(payload)
+    if (error != null)
+      return error
+    else
+      return new User(payload.id, payload.username, payload.name, "Sin bio")
+  }
+
+  static #checkPayload(payload) {
+    var error = { error: null }
+    if (payload == null) {
+      error.error = "payload no existe"
+      return error
+    } else if (payload.id == null || payload.username == null || payload.name == null) {
+      error.error = "necesitan tener un valor válido"
+      return error
+    } else {
+      return null
     }
-    return new User(payload.id, payload.username, payload.name, "Sin bio")
   }
 }
 
--- sprint_1/twitter/test/views/userView.test.js
+++ sprint_1/twitter/test/views/userView.test.js
@@ -7,4 +7,11 @@ describe("Tests for UserView", () => {
     
     expect(result.error).toMatch(/payload no existe/)
   });
+
+  test("Return error object when try to create user with a payload with invalid properties", () => {
+    const payload = {username: null, name: 12, id: "id"}
+    const result = UserView.createUser(payload)
+    
+    expect(result.error).toMatch(/necesitan tener un valor válido/)
+  });
 })
